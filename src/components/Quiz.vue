c7484dabbe5520b65f7aa9d137ce4b4a4d62c10e
PMunne <Pim.Munne@gmail.com>
2021-05-31 15:02:29 +0200

    made basic quiz layout

---
+++ src/components/Quiz.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <component v-bind:is="component" />
+    <button v-on:click="toggle">Toggle</button>
+  </div>
+</template>
+<script>
+import q0 from "./quiz-questions/q0";
+import q1 from "./quiz-questions/q1";
+export default {
+  name: "app",
+  components: {
+    q0,
+    q1,
+  },
+  data() {
+    return {
+      questionIndex: 0,
+      answers: [],
+      component: "q0",
+    };
+  },
+  methods: {
+    toggle() {
+      switch (this.questionIndex) {
+        case 0:
+          this.component = q1;
+          this.questionIndex++;
+          break;
+        case 1:
+          this.component = q0;
+          this.questionIndex++;
+          break;
+      }
+      
+    },
+  },
+};
+</script>
---
+++ src/components/quiz-questions/q0.vue
@@ -0,0 +1,85 @@
+<template>
+    <b-container class="p-4 quiz-background text-center">
+      <b-row class="d-flex justify-content-center">
+        <b-col class="col-6 col-md-2">
+          <div class="image-box">
+            <b-img
+              :src="strawberry"
+              alt="Image 1"
+              class="mx-auto d-block"
+            ></b-img>
+            <div class="text-background">
+            <p class="text-white">Aardbei</p>
+            </div>
+          </div>
+        </b-col>
+        <b-col class="col-6 col-md-2">
+          <div class="image-box">
+            <b-img
+              :src="strawberry"
+              alt="Image 2"
+              class="mx-auto d-block"
+            ></b-img>
+            <div class="text-background">
+            <p class="text-white">Aardbei</p>
+            </div>
+          </div>
+        </b-col>
+        <b-col class="col-6 col-md-2">
+          <div class="image-box">
+            <b-img
+              :src="strawberry"
+              alt="Image 3"
+              class="mx-auto d-block"
+            ></b-img>
+            <div class="text-background">
+            <p class="text-white">Aardbei</p>
+            </div>
+          </div>
+        </b-col>
+        <b-col class="col-6 col-md-2">
+          <div class="image-box">
+            <b-img
+              :src="strawberry"
+              alt="Image 3"
+              class="mx-auto d-block"
+            ></b-img>
+            <div class="text-background">
+            <p class="text-vertical-align">Aardbei</p>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+</template>
+<script>
+import strawberry from "../../assets/strawberry.png";
+export default {
+  name: "q0",
+  data: function () {
+    return {
+      strawberry: strawberry,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.image-box {
+  background-color: #262524;
+  border: 1px solid;
+  border-radius: 5px;
+}
+.text-background {
+  vertical-align: bottom;
+  background-color: rgb(44, 43, 43);
+}
+
+.quiz-background {
+  background-color: #403d3b;
+}
+.text-vertical-align {
+      display: flex;
+    align-items:center;
+}
+</style>
