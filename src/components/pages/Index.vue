3bec4a82d99c7d49d4f3d87257e037da3c63b29b
PMunne <Pim.Munne@gmail.com>
2021-06-01 12:40:37 +0200

    replaced header import in index instead of app

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,17 +1,20 @@
 <template>
   <b-container class="content">
     <b-row>
+      <b-col> <Header /></b-col>
       <b-col><Quiz /></b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import Quiz from "../Quiz"
+import Quiz from "../Quiz";
+import Header from "@/components/Header.vue";
 export default {
   name: "Index",
   components: {
-    Quiz
+    Quiz,
+    Header,
   },
 };
 </script>
