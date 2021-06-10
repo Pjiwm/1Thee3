b1e1dffce2ca7c02f085af77dbe0e89e33cdd6af
Mo-Gal <mo_@live.nl>
2021-06-10 10:27:00 +0200

    Added mailto href to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,49 +1,49 @@
 <template>
   <footer id="footer">
     <b-container>
-    <b-row class="py-4">
-      <b-col cols="6" md="8" lg="9">
-        <div class="padding-left-3">
-          <p>Kvk nummer | 17240186</p>
-          <p>Email | info@1t3</p>
-        </div>
-      </b-col>
-      <b-col
-        class="d-flex align-items-center flex-column"
-        cols="6"
-        md="4"
-        lg="3"
-      >
-        <div class="px-3">
-          <a href="/">
-          <img
-            class="img-fluid"
-            src="../../src/assets/1thee3logo.png"
-            width="90rem"
-          />
-          </a>
-          <div class="iconBox">
-            <a href="facebook.com">
-              <font-awesome-icon
-                class="icon p-1"
-                :icon="['fab', 'facebook-square']"
-              />
-            </a>
-            <a href="instagram.com">
-              <font-awesome-icon
-                class="icon p-1"
-                :icon="['fab', 'instagram']"
-              />
-            </a>
+      <b-row class="py-4">
+        <b-col cols="6" md="8" lg="9">
+          <div class="padding-left-3">
+            <p>Kvk nummer | 17240186</p>
+            <p>Email | <a href="mailto:info@1t3.nl">info@1t3.nl</a></p>
           </div>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col cols="12">
-        <p class="text-center">© IT3.nl | 2021</p>
-      </b-col>
-    </b-row>
+        </b-col>
+        <b-col
+          class="d-flex align-items-center flex-column"
+          cols="6"
+          md="4"
+          lg="3"
+        >
+          <div class="px-3">
+            <a href="/">
+              <img
+                class="img-fluid"
+                src="../../src/assets/1thee3logo.png"
+                width="90rem"
+              />
+            </a>
+            <div class="iconBox">
+              <a href="facebook.com">
+                <font-awesome-icon
+                  class="icon p-1"
+                  :icon="['fab', 'facebook-square']"
+                />
+              </a>
+              <a href="instagram.com">
+                <font-awesome-icon
+                  class="icon p-1"
+                  :icon="['fab', 'instagram']"
+                />
+              </a>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12">
+          <p class="text-center">© IT3.nl | 2021</p>
+        </b-col>
+      </b-row>
     </b-container>
   </footer>
 </template>
