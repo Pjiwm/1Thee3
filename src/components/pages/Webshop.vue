d241d171096182ff24c095b1f6667b859c0e1201
Mo-Gal <mo_@live.nl>
2021-06-10 11:24:22 +0200

    Added canonical URLs right syntax

--- src/components/pages/Webshop.vue
+++ src/components/pages/Webshop.vue
@@ -35,53 +35,12 @@ export default {
   metaInfo: {
     title: "1T3",
     titleTemplate: "%s - Bestel uw thee of theepakket in onze webshop",
+    link: [{ rel: "canonical", href="https://onet3.onrender.com/webshop" }],
+    meta: [
+      {name: "description", content: "Bestel uw thee of theepakket op 1Thee3.nl"},
+    ],
   },
-
-  meta: [
-    {
-      name: "description",
-      content: "Bestel uw thee of theepakket op 1Thee3.nl",
-    },
-    //SOCIALE MEDIA TAGS
-    // // OpenGraph data (Most widely used)
-    // { property: "og:title", content: "My Page Title ← My Site" },
-    // { property: "og:site_name", content: "My Site" },
-    // // The list of types is available here: http://ogp.me/#types
-    // { property: "og:type", content: "website" },
-    // // Should the the same as your canonical link, see below.
-    // { property: "og:url", content: "https://www.my-site.com/my-special-page" },
-    // {
-    //   property: "og:image",
-    //   content: "https://www.my-site.com/my-special-image.jpg",
-    // },
-    // // Often the same as your meta description, but not always.
-    // { property: "og:description", content: "I have things here on my site." },
-
-    // // Twitter card
-    // { name: "twitter:card", content: "summary" },
-    // {
-    //   name: "twitter:site",
-    //   content: "https://www.my-site.com/my-special-page",
-    // },
-    // { name: "twitter:title", content: "My Page Title ← My Site" },
-    // { name: "twitter:description", content: "I have things here on my site." },
-    // // Your twitter handle, if you have one.
-    // { name: "twitter:creator", content: "@alligatorio" },
-    // {
-    //   name: "twitter:image:src",
-    //   content: "https://www.my-site.com/my-special-image.jpg",
-    // },
-
-    // // Google / Schema.org markup:
-    // { itemprop: "name", content: "My Page Title ← My Site" },
-    // { itemprop: "description", content: "I have things here on my site." },
-    // {
-    //   itemprop: "image",
-    //   content: "https://www.my-site.com/my-special-image.jpg",
-    // },
-  ],
-  // Canonical url uitgecomment, wanneer domein beschikbaar is comment weghalen
-  link: [{ rel: "canonical", href="https://onet3.onrender.com/webshop" }],
+  
 
   created: function () {
     var scriptURL =
