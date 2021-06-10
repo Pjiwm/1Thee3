2645f6f72bc254aa9deb84b2c0b46ecd0c7c5461
Mo-Gal <mo_@live.nl>
2021-06-10 17:12:21 +0200

    Fixed router imports in main.js

--- src/main.js
+++ src/main.js
@@ -1,49 +1,48 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import './registerServiceWorker';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import '../src/scss/bootstrap.css';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import "./registerServiceWorker";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "../src/scss/bootstrap.css";
 // Import for fontAwesome : svg-core, brand icons and vue
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import VueMeta from 'vue-meta';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import VueMeta from "vue-meta";
 
 library.add(fab);
 // fontawesome component neccesary to call
-Vue.component('font-awesome-icon', FontAwesomeIcon, fab);
+Vue.component("font-awesome-icon", FontAwesomeIcon, fab);
 // Make BootstrapVue available throughout your project
-Vue.component('font-awesome-icon', FontAwesomeIcon, fab);
+Vue.component("font-awesome-icon", FontAwesomeIcon, fab);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueRouter);
 Vue.use(VueMeta);
 
-const Results = () =>
-    import ('./components/pages/Results')
-const Index = () =>
-    import ('./components/pages/Index')
-const PageNotFound = () =>
-    import ('./components/pages/PageNotFound')
+import results from "./components/pages/Results";
+import index from "./components/pages/Index";
+import pageNotFound from "./components/pages/PageNotFound";
+import webshop from "./components/pages/Webshop";
 
-const resultsRoute = { path: '/results', component: results };
-const indexRoute = { path: '/', component: index };
-const pageNotFoundRoute = { path: '*', component: pageNotFound };
-const webshopRoute = { path: '/webshop', component: webshop };
+const resultsRoute = { path: "/results", component: results };
+const indexRoute = { path: "/", component: index };
+const pageNotFoundRoute = { path: "*", component: pageNotFound };
+const webshopRoute = { path: "/webshop", component: webshop };
 
 const routes = [resultsRoute, indexRoute, pageNotFoundRoute, webshopRoute];
 
 const router = new VueRouter({
-    routes,
-    mode: 'history',
+  routes,
+  mode: "history",
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) },
-}).$mount('#app')
-
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");
