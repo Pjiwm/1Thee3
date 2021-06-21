d8a0b366d6d1abb804e8024c6f35cb6eca6807d8
Stijn van Houwelingen <stijn@van-houwelingen.net>
2021-06-21 11:31:26 +0200

    added checksum (program now does not download the same fotos twice). Also added limit on album requests.

--- php/renewToken.php
+++ php/renewToken.php
@@ -1,13 +1,18 @@
 <?php
 
+//Read existing token from token.txt
 $token_file = fopen("token.txt", "r") or die("Unable to open file!");
 $current_token = fread($token_file ,filesize("token.txt"));
 fclose($token_file);
+
+//Opens the file again, this time overwriting its contents
 $token_file = fopen("token.txt", "w+") or die("Unable to open file!");
 
+//Requests a new token
 $response = curlRequest("https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=" . $current_token);
 $new_token = $response['access_token'];
 
+//Writes the new token in the file
 fwrite($token_file, $new_token);
 fclose($token_file);
 
