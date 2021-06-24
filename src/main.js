7247d5fd00051dbca384c9bcb4c5c39385b75ecc
Stijn van Houwelingen <stijn@van-houwelingen.net>
2021-06-24 13:43:54 +0200

    selectively imported bootstrap, reduced css size by a lot

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import "./registerServiceWorker";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
 import "../src/scss/bootstrap.css";
 // Import for fontAwesome : svg-core, brand icons and vue
 import { library } from "@fortawesome/fontawesome-svg-core";
